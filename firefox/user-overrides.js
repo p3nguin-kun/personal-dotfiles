5424d8443b5681deeeacc2bca080e50dc054d2d9
p3nguin <p3nguinkun@proton.me>
2023-12-17 00:37:37 +0700

    add arkenfox overrides;

---
+++ firefox/user-overrides.js
@@ -0,0 +1,11 @@
+// Enable WebGL
+user_pref("webgl.disabled", true);
+
+// Embed video playback fix
+user_pref("network.http.referer.XOriginPolicy", 0);
+
+// Customize Firefox with FirefoxCSS
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Intergrated calculator at address bar
+user_pref("browser.urlbar.suggest.calculator", true);
