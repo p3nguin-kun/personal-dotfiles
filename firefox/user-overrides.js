70a888243626ac2bef1dbd3aff1177b8a86a6966
p3nguin <p3nguinkun@proton.me>
2024-01-08 23:00:24 +0700

    update arkenfox user.js overrides

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -1,9 +1,14 @@
-// Enable WebGL
+/* [ARKENFOX USER.JS OVERRIDES] */
+// 4520: enable WebGL
 user_pref("webgl.disabled", false);
 
-// Embed video playback fix
+// 5510: control when to send a cross-origin referer (embed video playback fix)
 user_pref("network.http.referer.XOriginPolicy", 0);
 
+// 5021: disable location bar using search
+user_pref("keyword.enabled", false);
+
+/* [OTHER SETTINGS] */
 // Customize Firefox with FirefoxCSS
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
